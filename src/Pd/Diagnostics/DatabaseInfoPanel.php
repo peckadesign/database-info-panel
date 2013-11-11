8dd3fc2080f860125edfc1e1819c21b21ae3678b
Milan Pála <milan.pala@email.cz>
2013-11-11 19:28:55 +0100

    Removed unused code

--- src/Pd/Diagnostics/DatabaseInfoPanel.php
+++ src/Pd/Diagnostics/DatabaseInfoPanel.php
@@ -47,7 +47,6 @@ class DatabaseInfoPanel implements Nette\Diagnostics\IBarPanel
 
 	public function getTab()
 	{
-		//$this->databaseParams = Nette\Environment::getConfig('database');
 		return '<span title="Database connection info">'
 		. '<img width=16 height=16 src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAEYSURBVBgZBcHPio5hGAfg6/2+R980k6wmJgsJ5U/ZOAqbSc2GnXOwUg7BESgLUeIQ1GSjLFnMwsKGGg1qxJRmPM97/1zXFAAAAEADdlfZzr26miup2svnelq7d2aYgt3rebl585wN6+K3I1/9fJe7O/uIePP2SypJkiRJ0vMhr55FLCA3zgIAOK9uQ4MS361ZOSX+OrTvkgINSjS/HIvhjxNNFGgQsbSmabohKDNoUGLohsls6BaiQIMSs2FYmnXdUsygQYmumy3Nhi6igwalDEOJEjPKP7CA2aFNK8Bkyy3fdNCg7r9/fW3jgpVJbDmy5+PB2IYp4MXFelQ7izPrhkPHB+P5/PjhD5gCgCenx+VR/dODEwD+A3T7nqbxwf1HAAAAAElFTkSuQmCC" />'
 		. "<span title=\"Database name\">{$this->getDatabaseName()}</span>"
